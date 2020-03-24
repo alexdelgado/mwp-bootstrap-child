27d3dae1d52362c0fb0c1655d7d90704ce046883
Alex Delgado <alex.delgado@fowardslash.com>
2020-03-23 19:30:38 -0500

    Abstracting the default WordPress REST API

--- functions.php
+++ functions.php
@@ -7,6 +7,10 @@ add_action( 'after_setup_theme', array( 'wp_bootstrap_theme', 'setup_theme' ) );
 require( 'inc/class.bootstrap-child-theme.php' );
 add_action( 'init', array( 'bootstrap_child_theme', 'singleton' ) );
 
+// WordPress REST API
+require( 'inc/class.bootstrap-child-rest-api.php' );
+add_action( 'init', array( 'bootstrap_child_rest_api', 'singleton' ) );
+
 // Accessible WordPress Menus
 require( get_template_directory() .'/inc/class.wp-bootstrap-walker-nav-menu.php' );
 require( 'inc/class.bootstrap-child-walker-nav-menu.php' );
