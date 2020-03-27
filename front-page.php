041b9641f6971e2fdd036674525abc94105cf25b
Alex Delgado <alex.delgado@fowardslash.com>
2020-03-27 13:43:19 -0500

    Adding hero block

---
+++ front-page.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+	<main class="main">
+		<section class="hero" style="background-image: url(<?php echo get_stylesheet_directory_uri() ?>/img/ave-calvar-dmwHW74iUzk-unsplash.jpg)">
+			<div class="hero__content">
+				<h2>Hero Title</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+			</div>
+		</section>
+	</main>
+
+<?php get_footer();
