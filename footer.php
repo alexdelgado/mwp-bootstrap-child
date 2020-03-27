636042d779066cf4459907dd486d0bc028eed226
Alex Delgado <alex.delgado@fowardslash.com>
2020-03-27 17:33:35 -0500

    Adding footer block

--- footer.php
+++ footer.php
@@ -16,7 +16,57 @@
 	</div>
 </section>
 
-<footer class="container-fluid"></footer>
+<footer class="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-8">
+				<nav>
+					<h2><?php echo wp_get_nav_menu_name('footer') ?></h2>
+					<?php
+						wp_nav_menu(
+							array(
+								'menu_class' => 'footer-menu',
+								'container' => false,
+								'items_wrap' => '<ul class="%2$s">%3$s</ul>',
+								'theme_location' => 'footer'
+							)
+						)
+					?>
+				</nav>
+			</div>
+			<div class="col-4">
+				<section class="subscribe">
+					<div class="subscribe__header">
+						<h2>Contact Us</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing.</p>
+					</div>
+					<form action="" class="subscribe__form">
+						<label for="subscribe" class="screen-reader-text">Subscribe to our newsletter</label>
+						<input type="text" id="subscribe" class="subscribe__email" placeholder="you@mail.com">
+					</form>
+					<div class="subscribe__social">
+						<a href="#" class="subscribe__network">
+							<span class="screen-reader-text">follow us on facebook</span>
+							<span class="lni lni-facebook"></span>
+						</a>
+						<a href="#" class="subscribe__network">
+							<span class="screen-reader-text">follow us on instagram</span>
+							<span class="lni lni-instagram"></span>
+						</a>
+						<a href="#" class="subscribe__network">
+							<span class="screen-reader-text">follow us on twitter</span>
+							<span class="lni lni-twitter"></span>
+						</a>
+						<a href="#" class="subscribe__network">
+							<span class="screen-reader-text">follow us on youtube</span>
+							<span class="lni lni-youtube"></span>
+						</a>
+					</div>
+				</section>
+			</div>
+		</div>
+	</div>
+</footer>
 
 <?php wp_footer(); ?>
 </body>
--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@ add_action( 'after_setup_theme', array( 'wp_bootstrap_theme', 'setup_theme' ) );
 
 require( 'inc/class.bootstrap-child-theme.php' );
 add_action( 'init', array( 'bootstrap_child_theme', 'singleton' ) );
+add_action( 'after_setup_theme', array( 'bootstrap_child_theme', 'setup_theme' ) );
 
 // WordPress REST API
 require( 'inc/class.bootstrap-child-rest-api.php' );
